298434b9c86ceb93167928739685e0f00ee8bddc
yu-sungho <sunghoyu.yeah@gmail.com>
2016-07-28 01:01:12 +0900

    Update PageTwo.js
    test

--- PageTwo.js
+++ PageTwo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import { Actions, ActionConst } from 'react-native-router-flux';
 import Button from 'react-native-button';
 
 export default class PageTwo extends Component {
@@ -8,7 +8,8 @@ export default class PageTwo extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-        pageTwoData : 'test'
+        loginMessage : this.props.loginMessage,
+        filterData: this.props.filterData,
     }
   }
 
@@ -17,19 +18,20 @@ export default class PageTwo extends Component {
   }
 
   _handlePress() {
-
+    Actions.pageThree({type: ActionConst.PUSH, filterData:this.state.filterData});
   }
 
     render() {
       return (
         <View style={{margin: 128}}>
-          <Text>{this.props.text}</Text>
-           <Button
+          <Text style={{fontSize:20}}>{this.props.loginMessage}</Text>
+          <Button
             containerStyle={{padding:10, height:45, overflow:'hidden', borderRadius:4, backgroundColor: 'green'}}
             style={{flex:1, fontSize: 20, color: '#FFF'}}
             onPress={() => this._handlePress()}>
-            Login Button
+            Go Detail View
            </Button>
+           <Text style={{fontSize:20}}>{this.props.filterData}</Text>
         </View>
       )
     }
